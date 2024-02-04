c23963d0d0b9b0e33e9aaecf395f84eb3cff3d23
alphaizm <46560526+alphaizm@users.noreply.github.com>
2024-02-05 00:41:09 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
-import usart
+  import usart
 import time
 
-port_name = "/dev/serial/by-id/PORTNAME"
+port_name = "/dev/serial0"
+#port_name = "/dev/serial/by-id/PORTNAME"
 cp = usart.CommunicationProtocol(port_name)
 
 Speed, Mode, ID, Acce, Brake_P = 150, 2, 1, 0, 0
